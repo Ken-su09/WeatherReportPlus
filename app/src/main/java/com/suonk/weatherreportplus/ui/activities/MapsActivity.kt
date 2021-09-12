6f84c3f10cea472aa9f8b8f343e190ab091a355f
Ken Suon <ken.su_dev@hotmail.com>
2021-09-12 23:27:33 +0200

    Add GoogleMaps to getCurrentLocation

---
+++ app/src/main/java/com/suonk/weatherreportplus/ui/activities/MapsActivity.kt
@@ -0,0 +1,115 @@
+package com.suonk.weatherreportplus.ui.activities
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.*
+
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.suonk.weatherreportplus.R
+import com.suonk.weatherreportplus.databinding.ActivityMapsBinding
+import com.suonk.weatherreportplus.utils.InjectorUtils
+import com.suonk.weatherreportplus.viewmodels.WeatherReportViewModel
+import java.util.*
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    private lateinit var lastLocation: Location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    companion object {
+        private const val LOCATION_REQUEST_CODE = 1
+    }
+
+    private lateinit var viewModel: WeatherReportViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        setUpViewModel()
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.uiSettings.isZoomControlsEnabled = true
+        setUpMap()
+    }
+
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_REQUEST_CODE
+            )
+            return
+        }
+        mMap.isMyLocationEnabled = true
+
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+            if (location != null) {
+                lastLocation = location
+
+                val currentLatLng = LatLng(lastLocation.latitude, lastLocation.longitude)
+                getCityFromLatLong(lastLocation.latitude, lastLocation.longitude)
+                placeMarkerOnMap(currentLatLng)
+
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+            }
+        }
+    }
+
+    private fun placeMarkerOnMap(currentLatLong: LatLng) {
+        val markerOptions = MarkerOptions().position(currentLatLong)
+        markerOptions.title("$currentLatLong")
+        mMap.addMarker(markerOptions)
+    }
+
+    private fun getCityFromLatLong(latitude: Double, longitude: Double) {
+        val geocoder = Geocoder(this, Locale.getDefault())
+        val listOfCitiesName = geocoder.getFromLocation(latitude, longitude, 1)
+        if (listOfCitiesName.size > 0) {
+            getCurrentWeatherByCurrentLocation(listOfCitiesName[0])
+        }
+    }
+
+    private fun setUpViewModel() {
+        val factory = InjectorUtils.provideViewModelFactory()
+        viewModel = ViewModelProvider(this, factory)[WeatherReportViewModel::class.java]
+    }
+
+    private fun getCurrentWeatherByCurrentLocation(address: Address?) {
+        viewModel.getCurrentWeatherByCurrentCity(address!!.locality)
+        viewModel.currentWeather.observe(this, { currentWeather ->
+            Log.i("getCurrentWeather", "${currentWeather.temperature}")
+            Log.i("getCurrentWeather", address.locality)
+        })
+    }
+}
