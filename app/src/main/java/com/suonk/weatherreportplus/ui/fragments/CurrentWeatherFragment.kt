1e41b6f1701280081fea6bd5fc62051481783947
Ken Suon <ken.su_dev@hotmail.com>
2021-09-14 21:52:47 +0200

    Correct infinite loop Retrofit Call

--- app/src/main/java/com/suonk/weatherreportplus/ui/fragments/CurrentWeatherFragment.kt
+++ app/src/main/java/com/suonk/weatherreportplus/ui/fragments/CurrentWeatherFragment.kt
@@ -13,9 +13,11 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
 import com.suonk.weatherreportplus.databinding.FragmentCurrentWeatherBinding
+import com.suonk.weatherreportplus.ui.activities.MainActivity
 import com.suonk.weatherreportplus.utils.CheckAndRequestPermissions.checkAndRequestPermission
 import com.suonk.weatherreportplus.viewmodels.WeatherReportViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,13 +26,13 @@ import java.util.*
 @AndroidEntryPoint
 class CurrentWeatherFragment : Fragment() {
 
+    //region ========================================== Val or Var ==========================================
+
     companion object {
         private const val REQUEST_LOCATION_PERMISSION = 10100
     }
 
     private var locationManager: LocationManager? = null
-
-    private val viewModel: WeatherReportViewModel by viewModels()
 
     private val locationListener: LocationListener = object : LocationListener {
         override fun onLocationChanged(location: Location) {
@@ -42,11 +44,13 @@ class CurrentWeatherFragment : Fragment() {
         override fun onProviderDisabled(provider: String) {}
     }
 
-    private lateinit var binding: FragmentCurrentWeatherBinding
+    private var cityName = ""
+    private val viewModel: WeatherReportViewModel by activityViewModels()
+    private var binding: FragmentCurrentWeatherBinding? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    //endregion
+
+    //region ============================================== UI ==============================================
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -54,7 +58,7 @@ class CurrentWeatherFragment : Fragment() {
     ): View {
         binding = FragmentCurrentWeatherBinding.inflate(inflater, container, false)
         initializeUI()
-        return binding.root
+        return binding!!.root
     }
 
     private fun initializeUI() {
@@ -63,19 +67,83 @@ class CurrentWeatherFragment : Fragment() {
 
         trackLocationIfPermissionIsGranted()
 
-        binding.buttonGetCurrentWeather.setOnClickListener {
-            binding.buttonGetCurrentWeather.isEnabled = false
-            buttonClickAnimation()
-            initProgressBar()
-            getLocationManager()
+        binding!!.buttonGetCurrentWeather.setOnClickListener {
+            getWeatherButtonClick()
+        }
+        moreDetailsButtonClick()
+    }
 
-            Handler(Looper.getMainLooper()).postDelayed({
-                binding.buttonGetCurrentWeather.isEnabled = true
-                binding.buttonGetCurrentWeatherGrey.isVisible = false
-                binding.buttonGetCurrentWeather.isVisible = true
-            }, 4000)
+    private fun initProgressBar() {
+        viewModel.loadingProgressBar.observe(viewLifecycleOwner, { isVisible ->
+            binding!!.progressBar.isVisible = isVisible
+            binding!!.weatherIcon.isVisible = !isVisible
+        })
+    }
+
+    private fun buttonClickAnimation() {
+        val frameAnimation = binding!!.buttonGetCurrentWeather.drawable as AnimationDrawable
+        frameAnimation.start()
+        Handler(Looper.getMainLooper()).postDelayed({
+            binding!!.buttonGetCurrentWeatherGrey.isVisible = true
+            binding!!.buttonGetCurrentWeather.isVisible = false
+            frameAnimation.stop()
+        }, 400)
+    }
+
+    //endregion
+
+    //region ========================================= Click Button =========================================
+
+    private fun moreDetailsButtonClick() {
+        binding!!.moreDetailsButton.setOnClickListener {
+            (activity as MainActivity).goToWeatherDetail()
+            Log.i("moreDetailsButtonClick", "${(activity as MainActivity).goToWeatherDetail()}")
         }
     }
+
+    private fun getWeatherButtonClick() {
+        binding!!.buttonGetCurrentWeather.isEnabled = false
+        buttonClickAnimation()
+        initProgressBar()
+        getCurrentWeatherByCurrentLocation(cityName)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            binding!!.buttonGetCurrentWeather.isEnabled = true
+            binding!!.buttonGetCurrentWeatherGrey.isVisible = false
+            binding!!.buttonGetCurrentWeather.isVisible = true
+        }, 4000)
+    }
+
+    //endregion
+
+    //region ======================================= Get Weather Data =======================================
+
+    private fun getCurrentWeatherByCurrentLocation(cityName: String) {
+        viewModel.getWeatherStackData(cityName)
+
+        binding!!.cityName.text = "City : $cityName"
+        viewModel.setLocationLiveData(cityName)
+
+        viewModel.weatherStackLiveData.observe(this, { weatherStackData ->
+            Log.i("getCurrentWeather", "${weatherStackData.current.temperature}")
+            Log.i("getCurrentWeather", "${weatherStackData.current.humidity}")
+
+            binding!!.weatherDescription.text = weatherStackData.current.weather_descriptions[0]
+            binding!!.temperatureValue.text = "${weatherStackData.current.temperature} °C"
+            binding!!.windValue.text = "${weatherStackData.current.wind_speed} km/h"
+            binding!!.humidityValue.text = "${weatherStackData.current.humidity} %"
+
+            Glide.with(this)
+                .load(weatherStackData.current.weather_icons[0])
+                .centerCrop()
+                .into(binding!!.weatherIcon)
+            binding!!.weatherIcon.visibility = View.VISIBLE
+        })
+    }
+
+    //endregion
+
+    //region ========================================= Get Location =========================================
 
     private fun getLocationManager() {
         try {
@@ -94,29 +162,8 @@ class CurrentWeatherFragment : Fragment() {
         val geocoder = Geocoder(context, Locale.getDefault())
         val listOfCitiesName = geocoder.getFromLocation(latitude, longitude, 1)
         if (listOfCitiesName.size > 0) {
-            getCurrentWeatherByCurrentLocation(listOfCitiesName[0])
+            cityName = listOfCitiesName[0].locality
         }
-    }
-
-    private fun getCurrentWeatherByCurrentLocation(address: Address?) {
-        viewModel.getWeatherStackData(address!!.locality)
-
-        binding.cityName.text = address.locality
-
-        viewModel.weatherStackLiveData.observe(this, { weatherStackData ->
-            Log.i("getCurrentWeather", "${weatherStackData.current.temperature}")
-            Log.i("getCurrentWeather", "${weatherStackData.current.humidity}")
-
-            binding.weatherDescription.text = weatherStackData.current.weather_descriptions[0]
-            binding.temperatureValue.text = "${weatherStackData.current.temperature} °C"
-            binding.windValue.text = "${weatherStackData.current.wind_speed} km/h"
-            binding.humidityValue.text = "${weatherStackData.current.humidity} %"
-
-            Glide.with(this)
-                .load(weatherStackData.current.weather_icons[0])
-                .centerCrop()
-                .into(binding.weatherIcon)
-        })
     }
 
     private fun trackLocationIfPermissionIsGranted() {
@@ -127,19 +174,14 @@ class CurrentWeatherFragment : Fragment() {
         )
     }
 
-    private fun initProgressBar() {
-        viewModel.loadingProgressBar.observe(this, { isVisible ->
-            binding.progressBar.isVisible = isVisible
-        })
+    //endregion
+
+    //region ========================================= Get Location =========================================
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 
-    private fun buttonClickAnimation() {
-        val frameAnimation = binding.buttonGetCurrentWeather.drawable as AnimationDrawable
-        frameAnimation.start()
-        Handler(Looper.getMainLooper()).postDelayed({
-            binding.buttonGetCurrentWeatherGrey.isVisible = true
-            binding.buttonGetCurrentWeather.isVisible = false
-            frameAnimation.stop()
-        }, 400)
-    }
+    //endregion
 }
