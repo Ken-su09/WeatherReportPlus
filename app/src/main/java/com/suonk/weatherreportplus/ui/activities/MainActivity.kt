d4156d701b589f49f1c5538b8b191fe1e6c999f7
Ken Suon <ken.su_dev@hotmail.com>
2021-09-14 21:54:22 +0200

    Add Coordinator in MainActivity

--- app/src/main/java/com/suonk/weatherreportplus/ui/activities/MainActivity.kt
+++ app/src/main/java/com/suonk/weatherreportplus/ui/activities/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.suonk.weatherreportplus.databinding.ActivityMainBinding
 import com.suonk.weatherreportplus.navigation.Navigator
+import com.suonk.weatherreportplus.navigation.WeatherReportFlowCoordinator
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -14,7 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var navigator: Navigator
-
+    private lateinit var coordinator: WeatherReportFlowCoordinator
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,15 +24,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         navigator.activity = this
+        coordinator = WeatherReportFlowCoordinator(navigator)
 
-        navigator.showSplashScreen()
+        coordinator.start()
         delayedToMainFragment()
     }
 
     private fun delayedToMainFragment() {
         Handler(Looper.getMainLooper()).postDelayed({
-            navigator.showCurrentWeather()
+            coordinator.showCurrentWeather()
         }, 4000)
+    }
+
+    fun goToWeatherDetail() {
+        coordinator.showWeatherDetails()
+    }
+
+    fun backToCurrentWeather() {
+        coordinator.showCurrentWeather()
     }
 
     override fun onDestroy() {
