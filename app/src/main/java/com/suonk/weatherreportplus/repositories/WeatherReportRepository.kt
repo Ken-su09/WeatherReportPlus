e5d0f8a9f934b76df1a752044107f5e0543b3756
Ken Suon <ken.su_dev@hotmail.com>
2021-10-01 13:14:00 +0200

    Add repository call to ViewModel, and ServiceCall to Repo

--- app/src/main/java/com/suonk/weatherreportplus/repositories/WeatherReportRepository.kt
+++ app/src/main/java/com/suonk/weatherreportplus/repositories/WeatherReportRepository.kt
@@ -2,21 +2,10 @@ package com.suonk.weatherreportplus.repositories
 
 import com.suonk.weatherreportplus.api.WeatherStackApiService
 import com.suonk.weatherreportplus.api.WeatherStackApiService.Companion.API_KEY
+import javax.inject.Inject
 
-class WeatherReportRepository constructor(private val weatherStackApiService: WeatherStackApiService) {
+class WeatherReportRepository @Inject constructor(private val weatherStackApiService: WeatherStackApiService) {
 
-//    suspend fun getWeatherStackData(city: String) =
-//        weatherStackApiService.getWeatherStackData(API_KEY, city)
-//
-//    companion object {
-//        @Volatile
-//        private var instance: WeatherReportRepository? = null
-//
-//        fun getInstance(weatherStackApiService: WeatherStackApiService): WeatherReportRepository =
-//            instance ?: synchronized(this) {
-//                instance ?: WeatherReportRepository(weatherStackApiService).also {
-//                    instance = it
-//                }
-//            }
-//    }
+    suspend fun getWeatherStackData(city: String) =
+        weatherStackApiService.getWeatherStackData(API_KEY, city)
 }
