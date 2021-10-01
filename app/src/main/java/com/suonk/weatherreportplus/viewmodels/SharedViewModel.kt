97833b576a949c693713c7a700f539d239ad9961
Ken Suon <ken.su_dev@hotmail.com>
2021-10-01 13:13:24 +0200

    Add Dispatchers.IO in ViewModel

--- app/src/main/java/com/suonk/weatherreportplus/viewmodels/SharedViewModel.kt
+++ app/src/main/java/com/suonk/weatherreportplus/viewmodels/SharedViewModel.kt
@@ -7,38 +7,35 @@ import androidx.lifecycle.viewModelScope
 import com.suonk.weatherreportplus.api.WeatherStackApiService
 import com.suonk.weatherreportplus.api.WeatherStackApiService.Companion.API_KEY
 import com.suonk.weatherreportplus.models.WeatherStackData
+import com.suonk.weatherreportplus.repositories.WeatherReportRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.*
 import javax.inject.Inject
 
 @HiltViewModel
 class SharedViewModel
-@Inject constructor(private val api: WeatherStackApiService) :
+@Inject constructor(private val repository: WeatherReportRepository) :
     ViewModel() {
 
-    var job: Job? = null
     val weatherStackLiveData = MutableLiveData<WeatherStackData>()
     val locationLiveData = MutableLiveData<String>()
     val loadingProgressBar = MutableLiveData<Boolean>()
 
     fun getWeatherStackData(city: String) {
-        job = viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             if (isActive) {
-                loadingProgressBar.value = true
-                val response = api.getWeatherStackData(API_KEY, city)
-                delay(3000)
                 withContext(Dispatchers.Main) {
-                    if (response.isSuccessful) {
-                        weatherStackLiveData.postValue(response.body())
-                        Log.i("ViewModel", "${response.body()}")
-                        Log.i("ViewModel", "$response")
-                        loadingProgressBar.value = false
-                    } else {
-                        Log.i("ViewModel", response.message())
-                    }
+                    loadingProgressBar.value = true
                 }
-
-                job?.cancel()
+                val response = repository.getWeatherStackData(city)
+                if (response.isSuccessful) {
+                    weatherStackLiveData.postValue(response.body())
+                    withContext(Dispatchers.Main) {
+                        loadingProgressBar.value = false
+                    }
+                } else {
+                    Log.i("ViewModel", response.message())
+                }
             }
         }
     }
@@ -46,10 +43,5 @@ class SharedViewModel
     fun setLocationLiveData(location: String) {
         locationLiveData.value = location
         Log.i("setLocationLiveData", location)
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        job?.cancel()
     }
 }
