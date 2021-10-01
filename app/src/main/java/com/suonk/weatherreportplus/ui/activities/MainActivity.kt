1433b776b57d5c38787ee30b20bf75d83af494f3
Ken Suon <ken.su_dev@hotmail.com>
2021-10-01 13:14:23 +0200

    Change Handler to CoroutineScope with Delay

--- app/src/main/java/com/suonk/weatherreportplus/ui/activities/MainActivity.kt
+++ app/src/main/java/com/suonk/weatherreportplus/ui/activities/MainActivity.kt
@@ -1,26 +1,15 @@
 package com.suonk.weatherreportplus.ui.activities
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
-import android.os.Build
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.google.android.gms.location.*
 import com.suonk.weatherreportplus.databinding.ActivityMainBinding
 import com.suonk.weatherreportplus.navigation.Navigator
 import com.suonk.weatherreportplus.navigation.WeatherReportFlowCoordinator
-import com.suonk.weatherreportplus.utils.CheckAndRequestPermissions.checkAndRequestPermission
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -39,16 +28,18 @@ class MainActivity : AppCompatActivity() {
         navigator.activity = this
         coordinator = WeatherReportFlowCoordinator(navigator)
 
-        Handler(Looper.getMainLooper()).postDelayed({
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(1500)
             coordinator.start()
-        }, 1500)
+        }
         delayedToMainFragment()
     }
 
     private fun delayedToMainFragment() {
-        Handler(Looper.getMainLooper()).postDelayed({
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(6000)
             coordinator.showCurrentWeather()
-        }, 6000)
+        }
     }
 
     fun goToWeatherDetail() {
